3eec30941ef33c3ac438ba8e4e9e14c0f26e720e
Tokki <hisokayamamaura@gmail.com>
2020-06-29 05:27:27 -0700

    final

--- frontend/src/page2/secondtable.tsx
+++ frontend/src/page2/secondtable.tsx
@@ -19,7 +19,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
-
+import {useState} from 'react';
+import axios from 'axios';
+import { gql } from 'apollo-boost';
+import { resolve } from 'dns';
+const getData = gql`
+  {nutrition {
+    id,name,calories,fat,carbs,protein
+  }}
+`
 interface Data {
   calories: number;
   carbs: number;
@@ -38,20 +46,7 @@ function createData(
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+var g_rows: { calories: number; carbs: number; fat: number; name: string; protein: number; }[] = [
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -242,6 +237,25 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function EnhancedTable() {
+
+  function getTable2Data() {
+    fetch("http://localhost:4999/graphql?query={nutrition{name,calories,fat,carbs,protein}}")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          g_rows.splice(0, g_rows.length)
+          result.data.nutrition.map((result: any) =>{
+            g_rows.push(createData(result.name, result.calories, result.fat, result.carbs, result.protein));
+          })
+          setRows(g_rows)
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+  
+  getTable2Data()
   const classes = useStyles();
   const [order, setOrder] = React.useState<Order>('asc');
   const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
@@ -249,6 +263,7 @@ export default function EnhancedTable() {
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState<{ calories: number; carbs: number; fat: number; name: string; protein: number; }[]>([]);
 
   const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof Data) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -258,7 +273,7 @@ export default function EnhancedTable() {
 
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
+      const newSelecteds = g_rows.map((n) => n.name);
       setSelected(newSelecteds);
       return;
     }
@@ -300,7 +315,7 @@ export default function EnhancedTable() {
 
   const isSelected = (name: string) => selected.indexOf(name) !== -1;
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, g_rows.length - page * rowsPerPage);
 
   return (
     <div className={classes.root}>
